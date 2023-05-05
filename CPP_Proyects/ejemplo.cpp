a92700c4d857fabb8099f400f151920190ee0df2
Lourdes90Cabrera <Lourdes90Cabrera@gmail.com>
2023-05-04 20:44:41 -0600

    actualizando ejemplo.cpp

--- CPP_Proyects/ejemplo.cpp
+++ CPP_Proyects/ejemplo.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-        
+
     /**
      * Seccion 01
      *  Alumnos : Lourdes Isabel Cabrera, Diana Alejandra Erazo Peres, Alejandra Iveth Escalante Menjivar
-    */
+     */
 
     // Declarando variables de entrada
 
@@ -20,7 +20,7 @@ int main()
 
     /* Se solicita al empleado que ingrese el salario ganado*/
 
-    cout << "Ingrese el Salario ganado :" <<endl;
+    cout << "Ingrese el Salario ganado :" << endl;
     cin >> salario;
 
     /* Se realiza la condición que evalua si el empleado ha ganado más de mil dolares en caso de ser así ,
@@ -30,10 +30,10 @@ int main()
     {
 
         Total_sav = salario * sav;
-        cout << "Se le ha aplicado un 0.30 al subsidio el total es de :  " << Total_sav <<endl;
+        cout << "Se le ha aplicado un 0.30 al subsidio el total es de :  " << Total_sav << endl;
 
-        cout << "Ingrese el genero al que pertenece :"<<endl;
-        cout << "[Femenino] o [Masculino] Ingrese una letra F o M" <<endl;
+        cout << "Ingrese el genero al que pertenece :" << endl;
+        cout << "[Femenino] o [Masculino] Ingrese una letra F o M" << endl;
         cin >> respuesta_genero;
 
         /* Se realiza la condición para determinar si el empleado es hombre o mujer
@@ -42,24 +42,24 @@ int main()
         if (respuesta_genero == 'F')
         {
 
-            cout << "Felicidades usted es apta para el ascenso al cargo de Jefa"<<endl;
+            cout << "Felicidades usted es apta para el ascenso al cargo de Jefa" << endl;
         }
         else
         {
-            cout << "Felicidades usted es apto para el ascenso al cargo de Jefe"<<endl;
+            cout << "Felicidades usted es apto para el ascenso al cargo de Jefe" << endl;
         }
 
         /* Luego se le pregunta al empleado si ha realizado ventas en el més en caso de que la respuesta sea positiva
         entonces se realiza la condición en la cual le pide que ingrese las ganancias totales*/
 
-        cout << "Usted ha generado ventas en el mes? "<<endl;
-        cout << "[SI] o [NO]"<<endl;
+        cout << "Usted ha generado ventas en el mes? " << endl;
+        cout << "[SI] o [NO]" << endl;
         cin >> respuesta_ventas;
 
         if (respuesta_ventas == 'S')
         {
 
-            cout << "Ingrese la ganancia total de las ventas realizadas"<<endl;
+            cout << "Ingrese la ganancia total de las ventas realizadas" << endl;
             cin >> ventas;
 
             /** se realiza la condicion que determinara si las ganancias totales son las requeridas para obtener un ascenso*/
@@ -67,128 +67,34 @@ int main()
             if (ventas >= 5000)
             {
 
-                cout << "Usted cumple con los criterios solicitados para el ascenso"<<endl;
+                cout << "Usted cumple con los criterios solicitados para el ascenso" << endl;
 
                 /** Se le pregunta si desea el cargo */
 
-                cout << "Desea aceptar el ascenso? "<<endl;
-                cout << "[SI] o [NO]"<<endl;
+                cout << "Desea aceptar el ascenso? " << endl;
+                cout << "[SI] o [NO]" << endl;
                 cin >> respuesta_usuario;
 
                 if (respuesta_usuario == 'S')
                 {
 
-                    cout << "Felicidades por haber obtenido el puesto de jefatura"<<endl;
+                    cout << "Felicidades por haber obtenido el puesto de jefatura" << endl;
                 }
                 else
                 {
-                    cout << "Usted no ha aceptado el puesto de jefatura"<<endl;
+                    cout << "Usted no ha aceptado el puesto de jefatura" << endl;
                 }
             }
             else
             {
 
-                cout << "Usted no cumple con los criterios solicitados para el ascenso"<<endl;
+                cout << "Usted no cumple con los criterios solicitados para el ascenso" << endl;
             }
         }
         else
         {
 
-            cout << "Su reporte ha finalizado"<<endl;
-        }
-    }
-    else
-    {
-
-        Total_suma_sav = salario * suma_sav;
-        cout << "Se le ha aplicado un 0.20 al subsidio el total es de :  " << Total_suma_sav <<endl;
-    }
-
-    cout << "Desea generar otro reporte? "<<endl;
-    cout << "[SI] o [NO]"<<endl;
-    cin >> respuesta;
-
-    if (respuesta == 'S') {
-
-    do
-    {
-            /* Se solicita al empleado que ingrese el salario ganado*/
-
-    cout << "Ingrese el Salario ganado : "<<endl;
-    cin >> salario;
-
-    /* Se realiza la condición que evalua si el empleado ha ganado más de mil dolares en caso de ser así ,
-    se le da un mensaje del tipo de descuento realizado y el subsidio total ganado*/
-
-    if (salario >= 10000)
-    {
-
-        Total_sav = salario * sav;
-        cout << "Se le ha aplicado un 0.30 al subsidio el total es de :  " << Total_sav<<endl;
-
-        cout << "Ingrese el genero al que pertenece "<<endl;
-        cout << "[Femenino] o [Masculino] Ingrese una letra F o M" <<endl;
-        cin >> respuesta_genero;
-
-        /* Se realiza la condición para determinar si el empleado es hombre o mujer
-        según la respuesta ingresada */
-
-        if (respuesta_genero == 'F')
-        {
-
-            cout << "Felicidades usted es apta para el ascenso al cargo de Jefa"<<endl;
-        }
-        else
-        {
-            cout << "Felicidades usted es apto para el ascenso al cargo de Jefe"<<endl;
-        }
-
-        /* Luego se le pregunta al empleado si ha realizado ventas en el més en caso de que la respuesta sea positiva
-        entonces se realiza la condición en la cual le pide que ingrese las ganancias totales*/
-
-        cout << "Usted ha generado ventas en el mes? "<<endl;
-        cout << "[SI] o [NO]"<<endl;
-        cin >> respuesta_ventas;
-
-        if (respuesta_ventas == 'S')
-        {
-
-            cout << "Ingrese la ganancia total de las ventas realizadas"<<endl;
-            cin >> ventas;
-
-            /** se realiza la condicion que determinara si las ganancias totales son las requeridas para obtener un ascenso*/
-
-            if (ventas >= 5000)
-            {
-
-                cout << "Usted cumple con los criterios solicitados para el ascenso"<<endl;
-
-                /** Se le pregunta si desea el cargo */
-
-                cout << "Desea aceptar el ascenso? "<<endl;
-                cout << "[SI] o [NO]"<<endl;
-                cin >> respuesta_usuario;
-
-                if (respuesta_usuario == 'S')
-                {
-
-                    cout << "Felicidades por haber obtenido el puesto de jefatura"<<endl;
-                }
-                else
-                {
-                    cout << "Usted no ha aceptado el puesto de jefatura"<<endl;
-                }
-            }
-            else
-            {
-
-                cout << "Usted no cumple con los criterios solicitados para el ascenso"<<endl;
-            }
-        }
-        else
-        {
-
-            cout << "Su reporte ha finalizado"<<endl;
+            cout << "Su reporte ha finalizado" << endl;
         }
     }
     else
@@ -198,18 +104,112 @@ int main()
         cout << "Se le ha aplicado un 0.20 al subsidio el total es de :  " << Total_suma_sav << endl;
     }
 
-    cout << "Desea generar otro reporte? "<<endl;
-    cout << "[SI] o [NO]"<<endl;
+    cout << "Desea generar otro reporte? " << endl;
+    cout << "[SI] o [NO]" << endl;
     cin >> respuesta;
-    
 
-    } while (respuesta =='S' || respuesta == 's') ;
-    system("Pause");
-    return 0;
-    
-    } else {
+    if (respuesta == 'S')
+    {
+
+        do
+        {
+            /* Se solicita al empleado que ingrese el salario ganado*/
+
+            cout << "Ingrese el Salario ganado : " << endl;
+            cin >> salario;
+
+            /* Se realiza la condición que evalua si el empleado ha ganado más de mil dolares en caso de ser así ,
+            se le da un mensaje del tipo de descuento realizado y el subsidio total ganado*/
+
+            if (salario >= 10000)
+            {
+
+                Total_sav = salario * sav;
+                cout << "Se le ha aplicado un 0.30 al subsidio el total es de :  " << Total_sav << endl;
+
+                cout << "Ingrese el genero al que pertenece " << endl;
+                cout << "[Femenino] o [Masculino] Ingrese una letra F o M" << endl;
+                cin >> respuesta_genero;
+
+                /* Se realiza la condición para determinar si el empleado es hombre o mujer
+                según la respuesta ingresada */
+
+                if (respuesta_genero == 'F')
+                {
+
+                    cout << "Felicidades usted es apta para el ascenso al cargo de Jefa" << endl;
+                }
+                else
+                {
+                    cout << "Felicidades usted es apto para el ascenso al cargo de Jefe" << endl;
+                }
+
+                /* Luego se le pregunta al empleado si ha realizado ventas en el més en caso de que la respuesta sea positiva
+                entonces se realiza la condición en la cual le pide que ingrese las ganancias totales*/
+
+                cout << "Usted ha generado ventas en el mes? " << endl;
+                cout << "[SI] o [NO]" << endl;
+                cin >> respuesta_ventas;
+
+                if (respuesta_ventas == 'S')
+                {
+
+                    cout << "Ingrese la ganancia total de las ventas realizadas" << endl;
+                    cin >> ventas;
+
+                    /** se realiza la condicion que determinara si las ganancias totales son las requeridas para obtener un ascenso*/
+
+                    if (ventas >= 5000)
+                    {
+
+                        cout << "Usted cumple con los criterios solicitados para el ascenso" << endl;
+
+                        /** Se le pregunta si desea el cargo */
+
+                        cout << "Desea aceptar el ascenso? " << endl;
+                        cout << "[SI] o [NO]" << endl;
+                        cin >> respuesta_usuario;
+
+                        if (respuesta_usuario == 'S')
+                        {
+
+                            cout << "Felicidades por haber obtenido el puesto de jefatura" << endl;
+                        }
+                        else
+                        {
+                            cout << "Usted no ha aceptado el puesto de jefatura" << endl;
+                        }
+                    }
+                    else
+                    {
+
+                        cout << "Usted no cumple con los criterios solicitados para el ascenso" << endl;
+                    }
+                }
+                else
+                {
+
+                    cout << "Su reporte ha finalizado" << endl;
+                }
+            }
+            else
+            {
+
+                Total_suma_sav = salario * suma_sav;
+                cout << "Se le ha aplicado un 0.20 al subsidio el total es de :  " << Total_suma_sav << endl;
+            }
+
+            cout << "Desea generar otro reporte? " << endl;
+            cout << "[SI] o [NO]" << endl;
+            cin >> respuesta;
+
+        } while (respuesta == 'S' || respuesta == 's');
+        system("Pause");
+        return 0;
+    }
+    else
+    {
 
         cout << "Muchas Gracias , Feliz Dia";
     }
-
 }
